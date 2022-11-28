090369bd44ef1fc4122eb4c3da5377ddb2cfb0b6
Audrey Warrene <101998231+arwarrene@users.noreply.github.com>
2022-11-28 15:00:03 -0500

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -2,9 +2,9 @@
 
 //variables to connect to database
 $host = "localhost";
-$username = "arw1957";
-$pass = "kyLA1dMFIb";
-$database_using = "arw1957";
+$username = "YOUR_USERNAME";
+$pass = "YOUR_PASSWORD";
+$database_using = "YOUR_DATABASE";
 
 //create a database instance
 $mysqli = new mysqli($host, $username, $pass, $database_using);
